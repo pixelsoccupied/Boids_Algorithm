f10eb9132983d52c7f7a6cbc07233b725d8a8021
pixelsoccupied <nahian@me.com>
2017-10-11 13:05:31 -0400

    Threaded Flocks now online

--- Boid.cpp
+++ Boid.cpp
@@ -7,6 +7,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
 //Seed and intialize only once
 std::random_device rd;
 std::mt19937 e2(rd());
@@ -86,6 +87,7 @@ Boid::Boid(float r, float g, float b) : r(r), g(g), b(b) {
 Boid::~Boid() {
 
 
+    std::cout << "boids dead " << std::endl;
 
 }
 
@@ -101,12 +103,15 @@ void Boid::setSpeed(float speed) {
 void Boid::drawBoid() {
 
 
+    Eigen::Vector2f temp;
+    temp(0) = x;
+    temp(1) = y;
 
     //std::cout<< velocity << "velocity! "<< std::endl;
 
     //move left n right
-    if (boidLeft) {
-        if (x > (-1 + 0.02)){
+   if (boidLeft) {
+        if (x > (-1 + 0.01)){
             x -= velocity.norm();
         }
         else {
@@ -114,7 +119,7 @@ void Boid::drawBoid() {
         }
     }
     else {
-        if (x < (1-0.02)) {
+        if (x < (1-0.01)) {
             x += velocity.norm();
         }
         else {
@@ -124,7 +129,7 @@ void Boid::drawBoid() {
 
     //move up n down
     if (boidDown) {
-        if (y > (-1+0.02)) {
+        if (y > (-1+0.01)) {
             y -= velocity.norm();
         }
         else {
@@ -132,28 +137,54 @@ void Boid::drawBoid() {
         }
     }
     else {
-        if (y < (1 - 0.02)) {
+        if (y < (1 - 0.01)) {
             y += velocity.norm();
         }
         else {
             boidDown = true;
         }
     }
+    position(0) = x;
+    position(1) = y;
 
+/*
+    dot = x1*x2 + y1*y2      # dot product between [x1, y1] and [x2, y2]
+    det = x1*y2 - y1*x2      # determinant
+    angle = atan2(det, dot)  # atan2(y, x) or atan2(sin, cos)*/
+
+    //activate for possible bonus marks
+
+    /*float dot = velocity.dot(position);
+    float det = (velocity(1)*position(0)) - (velocity(0)*position(1));
+    float rad = atan2(det, dot);*/
+
+
+
+    float result = (atan (abs(velocity(1))/abs(velocity(0)))) + (270 * (3.141592653589793238462643383279502884197169399375105820/180));
+
+    float degrees = (result * (180 /3.141592653589793238462643383279502884197169399375105820));
+
+    // std::cout << angle << std::endl;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0);
+    glRotatef(degrees, 0.0, 0.0, 1.0);
+    glTranslatef(-x, -y, 0);
 
 
 
     glColor3f(r, g, b);
     glBegin(GL_TRIANGLES);
+
     //glColor3f();
-    glVertex2f(0  + x, 0.02 + y);
-    glVertex2f(-0.02 + x,  -0.02 +y);
-    glVertex2f(0.02 + x, -0.02 + y);
+    glVertex2f(0  + x,0.01 + y);
+    glVertex2f(-0.01 + x,  -0.01 +y);
+    glVertex2f(0.01 + x, -0.01 + y);
 
     glEnd();
 
-    position(0) = x;
-    position(1) = y;
+    glPopMatrix();
+
 
 
 }
